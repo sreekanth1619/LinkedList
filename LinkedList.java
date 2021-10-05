b8d091f9294af57209c7c0b3e43455747e6a5810
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-05 12:17:42 +0530

    Adding: deleting the last element

---
+++ LinkedList.java
@@ -0,0 +1,62 @@
+package linkedlist;
+
+public class LinkedList {
+	 Node head;
+	    Node tail;
+
+	    public Node push(int data) {
+	        Node newNode = new Node(data);
+	        if (head == null) {
+	            head = newNode;
+	            tail = newNode;
+	        } else {
+	            Node temp = head;
+	            this.head = newNode;
+	            newNode.next = temp;
+	        }return newNode;
+	    }
+
+	    public void print() {
+	        if (head == null) {
+	            System.out.println("Linked List is Empty");
+	        } else {
+	            Node temp = head;
+	            while (temp != null) {
+	                if(temp.next != null)
+	                    System.out. print(temp.data + " -> ");
+	                else System.out.println(temp.data);
+	                temp = temp.next;
+	            }
+	        }
+	    }
+
+	    public void append(int data) {
+	        Node newNode = new Node(data);
+	        if (head == null) {
+	            head = newNode;
+	            tail = newNode;
+	        } else {
+	            this.tail.next = newNode;
+	            tail = newNode;
+	        }
+	    }
+
+	    public void insertInBetween(Node previousNode ,Node newNode){
+	        Node tempNode = previousNode.next;
+	        previousNode.next = newNode;
+	        newNode.next = tempNode;
+	    }
+
+	    public void pop() {
+	        this.head = this.head.next;
+	    }
+
+	    public void popLast() {
+	        Node tempNode= head;
+	        while(!tempNode.next.equals(tail)){
+	            tempNode=tempNode.next;
+	        }
+	        this.tail = tempNode;
+	        tempNode.next =null;
+	    }
+}
