ca7c67b9d9e657251d1e31aec4c53226d00411f1
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-05 12:00:45 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,43 @@
+package linkedlist;
+
+public class LinkedList {
+	Node head;
+    Node tail;
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            this.head = newNode;
+            newNode.next = temp;
+        }
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                if(temp.next != null)
+                    System.out. print(temp.data + " -> ");
+                else System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            tail = newNode;
+        }
+    }
+}
