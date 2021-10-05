551523ec719d8bfadac4aedcfeb1db2d002653c5
sreekanth1619 <87491860+sreekanth1619@users.noreply.github.com>
2021-10-05 12:20:59 +0530

    Adding: finding the node by searching the linked list

---
+++ LinkedList.java
@@ -0,0 +1,85 @@
+package linkedlist;
+
+public class LinkedList {
+	Node head;
+    Node tail;
+
+    public Node push(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            Node temp = head;
+            this.head = newNode;
+            newNode.next = temp;
+        }return newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        } else {
+            Node temp = head;
+            while (temp != null) {
+                if(temp.next != null)
+                    System.out. print(temp.data + " -> ");
+                else System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void insertInBetween(Node previousNode ,Node newNode){
+        Node tempNode = previousNode.next;
+        previousNode.next = newNode;
+        newNode.next = tempNode;
+    }
+
+    public void pop() {
+        this.head = this.head.next;
+    }
+
+    public void popLast() {
+        Node tempNode= head;
+        while(!tempNode.next.equals(tail)){
+            tempNode=tempNode.next;
+        }
+        this.tail = tempNode;
+        tempNode.next =null;
+    }
+
+    public void searchNode(int value) {
+        Node tempNode = head;
+        int index = 1;
+        boolean flag = false;
+        if (head == null) {
+            System.out.println("List is empty");
+        } else {
+            while (tempNode != null) {
+                if (tempNode.data == value) {
+                    flag = true;
+                    break;
+                }
+                index++;
+                tempNode = tempNode.next;
+            }
+        }
+        if (flag == true) {
+            System.out.println("Value:" + value + " is present at Index:" + index);
+        } else {
+            System.out.println("Element no present");
+        }
+    }
+}
